fc87e36a02e18e4e3b61e0a76f0668020ee0dd82
Sento Crespo <sentocrespo@gmail.com>
2024-10-20 12:02:58 +0200

    - Added more tests to cover the viewModel

--- App/Code/Project/Project/Classes/Scenes/HeroList/HeroListViewModel_SPEC.swift
+++ App/Code/Project/Project/Classes/Scenes/HeroList/HeroListViewModel_SPEC.swift
@@ -7,6 +7,7 @@ class HeroListViewModelTests: XCTestCase {
     
     // MARK: - Properties
     private var viewModel: HeroListViewModel!
+    private var render: HeroListRenderMock!
     private var cancellables: Set<AnyCancellable>!
     private let timeout: TimeInterval = 0.1
     
@@ -53,7 +54,7 @@ extension HeroListViewModelTests {
 
         wait(for: [expectation], timeout: timeout)
     }
-
+    
     /// GIVEN: An invalid local data source (file not found)
     /// WHEN: Calling the use case
     /// THEN: An error should be thrown and no data should be parsed
@@ -77,6 +78,65 @@ extension HeroListViewModelTests {
 
         wait(for: [expectation], timeout: timeout)
     }
+    
+    
+    /// GIVEN: A list of heroes
+    /// WHEN: Filtering by something that does NOT match anything
+    /// THEN: The list should be empty
+    func test_filterHeroes_no_results() {
+        // Given
+        let dataSource = UseCaseHeroDataSourceMock()
+            .withGetHeroes(.success([]))
+        viewModel = setupViewModel(dataSource: dataSource)
+        var hero1 = Hero.sample
+        hero1.name = "Spiderman"
+        var hero2 = Hero.sample
+        hero2.name = "Batman"
+        viewModel.model.heroes = [
+            hero1,
+            hero2
+        ]
+        let expectation = XCTestExpectation(description: "Filter heroes no results")
+        
+        // When
+        viewModel.model.searchText = "123"
+        viewModel.$model
+            .dropFirst() // Ignore initial state
+            .sink { model in
+                // Then
+                XCTAssertTrue(model.heroesFiltered.isEmpty)
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation], timeout: timeout)
+    }
+    
+    /// GIVEN: A list of heroes
+    /// WHEN: Selecting one of them
+    /// THEN: Details should be presented
+    func test_onSelectHero() {
+        // Given
+        let dataSource = UseCaseHeroDataSourceMock()
+            .withGetHeroes(.success([Hero.sample]))
+        viewModel = setupViewModel(dataSource: dataSource)
+        let expectation = XCTestExpectation(description: "Select hero")
+        
+        // When
+        viewModel.$model
+            .dropFirst() // Ignore initial state
+            .sink { [unowned self] model in
+                let hero = model.heroes.first!
+                self.viewModel.onSelect(hero: hero)
+                // Then
+                XCTAssertTrue(self.render.isToDetailsCalled)
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation], timeout: timeout)
+    }
+    
 }
 
 // MARK: - Private Methods
@@ -87,8 +147,9 @@ private extension HeroListViewModelTests {
             mainStore: AppReduxStore.mock(),
             heroDataSource: dataSource
         )
+        render = HeroListRenderMock(configurator: configurator)
         let viewModel = HeroListViewModel(
-            render: HeroListRenderMock(configurator: configurator),
+            render: render,
             configurator: configurator
         )
         return viewModel
