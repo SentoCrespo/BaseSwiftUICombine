e9ddb3b85035edc8de3e40c47b4c5a9091ff72aa
Sento Crespo <sentocrespo@gmail.com>
2024-11-06 12:08:46 +0100

    - Initial Widget configuration with Xcode Gen

--- App/Scripts/CodeGeneration/ProtocolsAutoGenerationCustom.swift
+++ App/Scripts/CodeGeneration/ProtocolsAutoGenerationCustom.swift
@@ -2,9 +2,6 @@ import Foundation
 
 // MARK: - Enabled protocols
 public protocol AutoInit {}
-public protocol AutoInitUndefined {}
-public protocol AutoInitUndefinedDecodable {}
-public protocol AutoInitAnyCodableDecodable {}
 
 // MARK: - Disabled protocols
 
