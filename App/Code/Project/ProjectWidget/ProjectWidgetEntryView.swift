870f3708dd2351d75d1a989f41618712004aae62
Sento Crespo <sentocrespo@gmail.com>
2024-11-06 12:16:14 +0100

    - Refactored Widget's architecture following project standards

---
+++ App/Code/Project/ProjectWidget/ProjectWidgetEntryView.swift
@@ -0,0 +1,17 @@
+import Foundation
+import WidgetKit
+import SwiftUI
+
+struct WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
